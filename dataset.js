ccbf7eb05edd534e8b4a6888ce54f7d57bfbcaba
Bugs Bunny <jiri.zajic@mensa.cz>
2015-10-02 16:29:37 +0200

    Results Q1 2015

--- dataset.js
+++ dataset.js
@@ -1,150 +1,26 @@
 var dataset = [
-    {
-    name: "barbora schnirchova",
-    value: 27
-},
-{
-    name: "honza dovrtel",
-    value: 21
-},
-{
-    name: "stepan cenek",
-    value: 19
-},
-{
-    name: "marcela vrbkova",
-    value: 15
-},
-{
-    name: "adrian toman",
-    value: 15
-},
-{
-    name: "pavlina orgonikova",
-    value: 10
-},
-{
-    name: "milan koudelka",
-    value: 9
-},
-{
-    name: "miroslav spousta",
-    value: 9
-},
-{
-    name: "petr benas",
-    value: 9
-},
-{
-    name: "jakub plichta",
-    value: 6
-},
-{
-    name: "petr chlupaty",
-    value: 5
-},
-{
-    name: "petr klemsinsky",
-    value: 5
-},
-{
-    name: "jan basko",
-    value: 5
-},
-{
-    name: "richard benkovsky",
-    value: 5
-},
-{
-    name: "eva dvorakova",
-    value: 5
-},
-{
-    name: "ondrej cernos",
-    value: 5
-},
-{
-    name: "vojtech jasny",
-    value: 4
-},
-{
-    name: "martin tosovsky",
-    value: 3
-},
-{
-    name: "zdenek macicek",
-    value: 3
-},
-{
-    name: "leos flodr",
-    value: 3
-},
-{
-    name: "martin caslavsky",
-    value: 3
-},
-{
-    name: "tomas jirotka",
-    value: 3
-},
-{
-    name: "renata bordovska",
-    value: 2
-},
-{
-    name: "ondrej matejka",
-    value: 2
-},
-{
-    name: "martin matula",
-    value: 2
-},
-{
-    name: "lubomir sedlacik",
-    value: 2
-},
-{
-    name: "libor rysavy",
-    value: 2
-},
-{
-    name: "matej hasul",
-    value: 2
-},
-{
-    name: "guilherme pereira",
-    value: 2
-},
-{
-    name: "tomas korcak",
-    value: 2
-},
-{
-    name: "jiri sitina",
-    value: 1
-},
-{
-    name: "jiri zajic",
-    value: 1
-},
-{
-    name: "david jez",
-    value: 1
-},
-{
-    name: "michal pesicka",
-    value: 1
-},
-{
-    name: "daryna skryl",
-    value: 1
-},
-{
-    name: "vojtech pasler",
-    value: 1
-},
-{
-    name: "adam tkac",
-    value: 1
-}
+    { name: 'adrian toman', value: 20 },
+    { name: 'barbora schnirchova', value: 17 },
+    { name: 'tereza stovickova', value: 15 },
+    { name: 'miroslav spousta', value: 12 },
+    { name: 'martin tosovsky', value: 4 },
+    { name: 'marcela vrbkova', value: 4 },
+    { name: 'jiri stovicek', value: 3 },
+    { name: 'michal pesicka', value: 3 },
+    { name: 'petr klemsinsky', value: 3 },
+    { name: 'jiri sitina', value: 2 },
+    { name: 'jakub jedelsky', value: 2 },
+    { name: 'matej hasul', value: 2 },
+    { name: 'honza', value: 2 },
+    { name: 'stepan cenek', value: 2 },
+    { name: 'martin caslavsky', value: 2 },
+    { name: 'david jez', value: 1 },
+    { name: 'dusan katona', value: 1 },
+    { name: 'pavol gressa', value: 1 },
+    { name: 'peter plochan', value: 1 },
+    { name: 'petr benes', value: 1 },
+    { name: 'adam tkac', value: 1 },
+    { name: 'jiri zajic', value: 1 },
+    { name: 'michal marsal', value: 1 },
+    { name: 'vojtech rylko', value: 1 }
 ];
